485514c5a18bce9b3572809064c351631ae628e1
HARD SMAIL <abobusrm@gmail.com>
2022-04-06 16:51:16 +0300

    make it better

--- telega.py
+++ telega.py
@@ -6,7 +6,7 @@ api_id = 9623699
 api_hash = 'fe0f2a7acd200c8fa21a0aaa59b59ae2'
 
 my_channel_id = -1001765381029
-channels = [-1001355465930, -1001370708606, -1001201979273, -1001286238248, -1001790607385, -1001728917276, -1001366535587, -1001470142620]
+channels = [-1001566434690, -1001728917276]
 
 client = TelegramClient('myGrab', api_id, api_hash)
 print("buma top")
@@ -14,7 +14,7 @@ print("buma top")
 @client.on(events.NewMessage(chats=channels))
 async def my_event_handler(event): 
   if event.message:
-    match = re.findall(r"(?<!\/)\b(?=[0-9]*[A-Z])[A-Z0-9]+\b", event.message.message)
+    match = re.findall(r"(?<!\/)\b(?=[0-9]*[A-Z*_])[A-Z0-9_]+\b", event.message.message)
     if not match:
       return
     await client.send_message(my_channel_id, match[0])
